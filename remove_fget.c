d7d8b55596f95d0a089f6797c2a15081819f5f11
TogVinh <tongthinhvinh12@gmail.com>
2024-04-03 22:10:30 +0700

    test ssh with remove_fget

--- remove_fget.c
+++ remove_fget.c
@@ -1,2 +1,2 @@
 // remove "\n" form fgets 
-#define strcut_newline(str) ((str)[strlen(str) - 1] == '\n')? (str)[strlen(str) - 1] = '\0':0;
+#define strcut_newline(str) ((str)[strlen(str) - 1] == '\n')? (str)[strlen(str) - 1] = '\0': 0;
